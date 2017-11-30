a06297a0397b53863b70ed2e4093e8126fba23a1
Ddc <decraemerdante@gmail.com>
2017-11-30 11:25:45 +0100

    lol

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cities: []
+        };
+}
+
+    componentDidMount(){
+        fetch('http://cunning-convoys.azurewebsites.net/api/Cities')
+         .then(response => response.json())
+            .then(
+                data => {
+                    console.log(data);
+                    this.setState({cities: data});
+                }
+
+            );
+
+    }
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ul>
+          {
+              this.state.cities.map(function(city, i) {
+                  return <li key={i}>{city.name}</li>
+              })
+          }
+      </ul>
       </div>
+
+
     );
   }
 }
