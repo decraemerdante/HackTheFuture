da3d6be7685bc2875469220e05f4e1b4cd0b158d
arnoutborn <arnout.born@gmail.com>
2017-11-30 12:26:49 +0100

    update data

--- Second/assets/js/js.js
+++ Second/assets/js/js.js
@@ -5,12 +5,27 @@ var cities;
 var convoys;
 
 $(document).ready(function () {
+
+    $("nav").on("click", "div.HomeLink" ,function () {
+        location.reload();
+    });
+    $("nav").on("click", "div.UpdateLink" ,function () {
+        getCities();
+        getConvoys();
+    });
+
+
+    initPage();
     getCities();
     getConvoys();
 
 
 
 });
+
+function initPage() {
+    $("nav").append("")
+}
 
 function getCities() {
 
