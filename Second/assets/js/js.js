5e79ce94d8f0e0558cf712401c4e19526ed0549a
arnout born <arnout.born@gmail.com>
2017-11-30 11:30:22 +0100

    jquery bitches

--- Second/assets/js/js.js
+++ Second/assets/js/js.js
@@ -5,11 +5,27 @@
 
 $(document).ready(() => {
 
+
+    GetData();
+
     console.log("test");
 
     $("#container").append("<p>test</p>");
 
+});
+
+function GetData() {
+
+    $.ajax({
+        url: "http://cunning-convoys.azurewebsites.net/api/cities",
+        context: document.body
+    }).done(function() {
+        $( "#container").append( result );
     });
+
+}
+
+
 
 function LoadData() {
     return "test";
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Cities from './Cities';
-
-ReactDOM.render(<Cities />, document.getElementById('root'));
