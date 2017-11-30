785803a53ac3d68ef5e6d1ed8b2758a9e0a027b3
Ddc <decraemerdante@gmail.com>
2017-11-30 11:57:37 +0100

    show city names

--- Second/assets/js/js.js
+++ Second/assets/js/js.js
@@ -1,31 +1,44 @@
 /**
  * Created by Panda on 30/11/2017.
  */
+var cities;
 
-
-$(document).ready(() => {
-
-
+$(document).ready(function () {
     GetData();
+    showCityNames();
 
     console.log("test");
 
     $("#container").append("<p>test</p>");
-
 });
 
 function GetData() {
 
     $.ajax({
-        url: "http://cunning-convoys.azurewebsites.net/api/cities",
-        context: document.body
-    }).done(function() {
-        $( "#container").append( result );
+
+        url: 'http://cunning-convoys.azurewebsites.net/api/Cities',
+        type: "GET",
+        dataType: "json",
+        data: {
+            format: "json"
+        },
+        success: function (data) {
+          cities = data;
+        },
+        error: function (xhr, message) {
+            console.log(xhr, message);
+        }
+
     });
 
 }
 
-
+function showCityNames(){
+    $("#container").append("<ul class='test'></ul>");
+    cities.forEach(function (city) {
+        $(".test").append("<li>" + city.name + "</li>");
+    })
+}
 
 function LoadData() {
     return "test";
